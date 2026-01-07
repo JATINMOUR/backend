9042e6a7cb1971f32294824448e6391e09467b7f
JΛƬIП Mourya <102876688+JATINMOUR@users.noreply.github.com>
2026-01-07 10:37:54 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,33 +1,9 @@
 import express from "express";
 import cors from "cors";
 import reviewRoutes from "./routes/reviewRoutes.js";
-<<<<<<< HEAD
-import connectDB from "./config/db.js";
-
-const app = express();
-
-// middlewares
-app.use(cors());
-app.use(express.json());
-
-// DB connection (safe for both)
-connectDB();
-
-// routes
-app.use("/api/reviews", reviewRoutes);
-
-// health check
-app.get("/", (req, res) => {
-  res.status(200).json({ status: "API running successfully 🚀" });
-});
-
-=======
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/reviews", reviewRoutes);
-
->>>>>>> a723c0b (Initial cinematic portfolio setup)
-export default app;

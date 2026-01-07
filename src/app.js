5f2b05785a23427a561d73de4b1c793e87f9878f
JATINMOUR <jateen1231@gmail.com>
2026-01-07 10:29:13 +0530

    Initial cinematic portfolio setup

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import reviewRoutes from "./routes/reviewRoutes.js";
+<<<<<<< HEAD
 import connectDB from "./config/db.js";
 
 const app = express();
@@ -20,4 +21,13 @@ app.get("/", (req, res) => {
   res.status(200).json({ status: "API running successfully 🚀" });
 });
 
+=======
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/reviews", reviewRoutes);
+
+>>>>>>> a723c0b (Initial cinematic portfolio setup)
 export default app;
